70ddce21398172964f7c4873836228de4a5fe1a0
Telework Inc <hello@trytelework.com>
2020-05-09 20:37:59 -0500

    BULK AUTOMATIC COMMIT

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const page2 = () => (<><Layout><SEO title="Page two" /><h1>Hi from the second page</h1><p>Welcome to Page 2</p><Link to="/">Go back to the homepage</Link></Layout></>);
 
